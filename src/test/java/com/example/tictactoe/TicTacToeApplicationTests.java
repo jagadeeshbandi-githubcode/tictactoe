781550235d609a5a5c104455330d17d5b55b1825
Bandi <jagadeesh.bandi@capgemini.com>
2022-11-30 11:29:17 +0530

    Import issue fix for main class

--- src/test/java/com/example/tictactoe/TicTacToeApplicationTests.java
+++ src/test/java/com/example/tictactoe/TicTacToeApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.tictactoe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TicTacToeApplicationTests {
 
 	@Test
-    void test()
+	void test()
     {
 		TicTacToeApplication.main(new String[]{
                 "--spring.main.web-environment=false",
